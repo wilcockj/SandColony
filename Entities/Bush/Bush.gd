1ed99cd27aeac79ea14aa8070964eb522a9032a2
James Wilcock <jameswilcock8@gmail.com>
2024-08-31 18:06:13 -0700

    Added grow sticks back timer to bush

--- Entities/Bush/Bush.gd
+++ Entities/Bush/Bush.gd
@@ -2,6 +2,7 @@ class_name Bush extends StaticBody3D
 @export var stick_count: int = 5  # Number of sticks available in the bush
 var cur_stick_count = stick_count
 @export var text_label: Sprite3D
+@onready var grow_timer: Timer = %GrowTimer
 
 func _ready():
 	text_label.Set_3D_Text("Bush " + str(cur_stick_count) + "/" + str(stick_count) + " sticks")
@@ -15,7 +16,15 @@ func harvest_stick() -> bool:
 			text_label.Set_3D_Text("Bush " + str(cur_stick_count) + "/" + str(stick_count) + " sticks")
 		return true
 	else:
+		print("starting grow timer")
+		grow_timer.start()
 		return false
 
 func has_work() -> bool:
 	return cur_stick_count > 0
+
+
+func _on_grow_timer_timeout() -> void:
+	print("Grew sticks back")
+	cur_stick_count = stick_count
+	text_label.Set_3D_Text("Bush " + str(cur_stick_count) + "/" + str(stick_count) + " sticks")
